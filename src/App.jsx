ef7adf0ed505b1983cb47ba113739214aa80e579
Ramayanamsarayu <85180908+Ramayanamsarayu@users.noreply.github.com>
2024-02-08 11:49:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Studentui from "./components/component/Student/studentui";
 
 import React from "react";
