bc8de790fc0ec62338828e846a601f9112d353cc
Varun-L <51707813+Varun-L@users.noreply.github.com>
2024-03-07 00:21:58 +0000

    auth0init

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Auth0Provider
+    domain="zettabyte.us.auth0.com"
+    clientId="rdSr4vMEjJGUNwGjq9M8TEnrhw1oiYvM"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
+  </Auth0Provider>,
   </React.StrictMode>,
 )
