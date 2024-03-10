9fbcb1d5d4f2c680bf0993bc766e0873184df27f
Kaif <kaifata31@gmail.com>
2024-03-11 00:48:33 +0530

    Updated

--- src/components/component/Auth/BaseAuth.jsx
+++ src/components/component/Auth/BaseAuth.jsx
@@ -2,8 +2,15 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import Loader from "@/components/ui/Loader/Loader.jsx";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
 
 function BaseAuth() {
+  const [typeEffect] = useTypewriter({
+    words: ["Welcome To The UMS", "Connect With Your Classroom "],
+    loop: {},
+    typeSpeed: 120,
+    deleteSpeed: 70,
+  });
   const { loginWithRedirect } = useAuth0();
   const { user, isAuthenticated, isLoading } = useAuth0();
   let history = useNavigate();
@@ -33,9 +40,13 @@ function BaseAuth() {
   return (
     <>
       <div className="h-screen bg-[#040404] text-white flex items-center justify-center">
-        <div className=" text-center px-10 py-10">
-          <h1 className="text-[5vw] font-bold leading-none">
-            Welcome to the <span className="text-[#FFD31A]">UMS</span>
+        <div className=" text-center px-10 py-10 box">
+          <h1 className="text-[5vw] font-bold leading-none text-[#ffd11abe]">
+            {typeEffect}
+
+            <span>
+              <Cursor />
+            </span>
           </h1>
           <h1 className="text-[3vw] font-semibold text-zinc-400 leading-none mt-5">
             University Management System
@@ -49,7 +60,7 @@ function BaseAuth() {
 
           <div className="flex items-center justify-center mt-10">
             <button
-              className="bg-[#ffd11aab] hover:bg-[#FFD31A] text-black outline-none px-6 py-3 rounded-md font-semibold text-xl"
+              className="bg-[#ffd11aab] hover:bg-[#FFD31A] text-black outline-none px-20 py-3 rounded-md font-semibold text-xl"
               onClick={() => loginWithRedirect()}
             >
               Log in
